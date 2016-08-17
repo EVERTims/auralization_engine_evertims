6206b6efc325d508a2f923d59210a486efea0a7b
David Poirier-Quinot <davipoir@gmail.com>
2016-08-17 23:00:15 +0200

    replaced {AudioInputComponent.cpp, AudioInputComponent.h} by {AudioInputComponent.h} only

--- Source/AudioInputComponent.h
+++ Source/AudioInputComponent.h
@@ -1,39 +1,17 @@
-#pragma once
-
 #include "../JuceLibraryCode/JuceHeader.h"
 
-
 class AudioInputComponent:
-	public Component,
-    public Button::Listener,
-    public ChangeListener
+public Component,
+public Button::Listener,
+public ChangeListener
 {
-
+    
+    //==========================================================================
     
 public:
-
-    // constructor & destructor
-	AudioInputComponent();
-	~AudioInputComponent();
-    
-    // weird non yet overrided methods (kept the name because it's what they do..)
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill);
-    
-    // attributes
     AudioTransportSource transportSource;
-
     
 private:
-    
-    // overrided JUCE methods
-    void paint(Graphics& g) override;
-    void resized() override;
-    void buttonClicked (Button* button) override;
-    void changeListenerCallback (ChangeBroadcaster* source) override;
-    
-    // audio file reader methods
-    bool openAudioFile();
-    
     // audio file reader attributes
     bool shouldLoopAudioFile = false;
     AudioFormatManager formatManager;
@@ -47,7 +25,6 @@ private:
         Stopping
     };
     TransportState audioPlayerState;
-    void changeState (TransportState newState);
     
     // GUI elements
     TextButton audioFileOpenButton;
@@ -57,5 +34,223 @@ private:
     Label audioFileCurrentPositionLabel;
     Slider gainMasterSlider;
     
+    //==========================================================================
+    
+public:
+    
+    AudioInputComponent()
+    {
+        //==========================================================================
+        // INIT AUDIO READER ELEMENTS
+        
+        formatManager.registerBasicFormats();
+        transportSource.addChangeListener (this);
+        
+        //==========================================================================
+        // INIT GUI ELEMENTS
+        
+        addAndMakeVisible (&audioFileOpenButton);
+        audioFileOpenButton.setButtonText ("Open...");
+        audioFileOpenButton.addListener (this);
+        
+        addAndMakeVisible (&audioFilePlayButton);
+        audioFilePlayButton.setButtonText ("Play");
+        audioFilePlayButton.addListener (this);
+        audioFilePlayButton.setColour (TextButton::buttonColourId, Colours::green);
+        audioFilePlayButton.setEnabled (false);
+        
+        addAndMakeVisible (&audioFileStopButton);
+        audioFileStopButton.setButtonText ("Stop");
+        audioFileStopButton.addListener (this);
+        audioFileStopButton.setColour (TextButton::buttonColourId, Colours::red);
+        audioFileStopButton.setEnabled (false);
+        
+        addAndMakeVisible (&gainMasterSlider);
+        gainMasterSlider.setRange(0.1, 4.0);
+        gainMasterSlider.setValue(1.0);
+        gainMasterSlider.setSliderStyle(Slider::LinearHorizontal);
+        gainMasterSlider.setColour(Slider::textBoxBackgroundColourId, Colours::transparentBlack);
+        gainMasterSlider.setColour(Slider::textBoxTextColourId, Colours::white);
+        gainMasterSlider.setColour(Slider::textBoxOutlineColourId, Colours::transparentBlack);
+        gainMasterSlider.setTextBoxStyle(Slider::TextBoxRight, true, 70, 20);
+        
+        addAndMakeVisible (&audioFileLoopToogle);
+        audioFileLoopToogle.setButtonText ("Loop");
+        audioFileLoopToogle.setColour(ToggleButton::textColourId, Colours::whitesmoke);
+        audioFileLoopToogle.setEnabled(true);
+        audioFileLoopToogle.addListener (this);
+        
+        addAndMakeVisible (&audioFileCurrentPositionLabel);
+        audioFileCurrentPositionLabel.setText ("Making progress, every day :)", dontSendNotification);
+        audioFileCurrentPositionLabel.setColour(Label::textColourId, Colours::whitesmoke);
+        
+    }
+    
+    ~AudioInputComponent() {}
+    
+    //==========================================================================
+    // GUI METHODS
+    
+    bool openAudioFile()
+    {
+        bool fileOpenedSucess = false;
+        
+        FileChooser chooser ("Select a Wave file to play...",
+                             File::nonexistent,
+                             "*.wav");
+        
+        if (chooser.browseForFileToOpen())
+        {
+            File file (chooser.getResult());
+            AudioFormatReader* reader = formatManager.createReaderFor (file);
+            
+            if (reader != nullptr)
+            {
+                ScopedPointer<AudioFormatReaderSource> newSource = new AudioFormatReaderSource (reader, true);
+                transportSource.setSource (newSource, 0, nullptr, reader->sampleRate);
+                readerSource = newSource.release();
+                fileOpenedSucess = true;
+            }
+        }
+        return fileOpenedSucess;
+    }
+    
+    
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+    {
+        // check if audiofile loaded
+        if (readerSource == nullptr)
+        {
+            bufferToFill.clearActiveBufferRegion();
+            return;
+        }
+        
+        // fill buffer with audiofile data
+        transportSource.getNextAudioBlock (bufferToFill);
+        
+        // stereo downmix to mono
+        bufferToFill.buffer->applyGain(0.5f);
+        bufferToFill.buffer->addFrom(0, 0, bufferToFill.buffer->getWritePointer(1), bufferToFill.buffer->getNumSamples());
+        
+        // apply master gain
+        bufferToFill.buffer->applyGain(gainMasterSlider.getValue());
+    }
+    
+    //==========================================================================
+    //==========================================================================
+    
+private:
+    
+    //==========================================================================
+    // GUI METHODS
+    
+    void paint(Graphics& g) override
+    {
+        float w = static_cast<float>( getWidth() );
+        float h = static_cast<float>( getHeight() );
+        
+        g.setOpacity(1.0f);
+        g.setColour(Colours::whitesmoke);
+        g.drawRoundedRectangle(0.f, 0.f, w, h, 10.0f, 3.0f);
+    }
+    
+    void resized() override
+    {
+        auto pos = getPosition();
+        
+        DBG(String(pos.getY() + 10));
+        
+        int thirdWidth = (int)(getWidth() / 3) - 20;
+        audioFileOpenButton.setBounds (20, pos.getY() + 10, thirdWidth, 40);
+        audioFilePlayButton.setBounds (30 + thirdWidth, pos.getY() + 10, thirdWidth, 40);
+        audioFileStopButton.setBounds (40 + 2*thirdWidth, pos.getY() + 10, thirdWidth, 40);
+        audioFileLoopToogle.setBounds (10, pos.getY() + 60, getWidth() - thirdWidth, 30);
+        
+        gainMasterSlider.setBounds    (10, pos.getX() + 100, getWidth() - 20, 20);
+        
+        audioFileCurrentPositionLabel.setBounds (10, 130, getWidth() - 20, 20);
+    }
+    
+    //==========================================================================
+    // LISTENER METHODS
+    
+    void buttonClicked (Button* button) override
+    {
+        if (button == &audioFileOpenButton)
+        {
+            bool fileOpenedSucess = openAudioFile();
+            audioFilePlayButton.setEnabled (fileOpenedSucess);
+        }
+        
+        if (button == &audioFilePlayButton)
+        {
+            if (readerSource != nullptr)
+                readerSource->setLooping (audioFileLoopToogle.getToggleState());
+            changeState(Starting);
+        }
+        
+        if (button == &audioFileStopButton)
+        {
+            changeState (Stopping);
+        }
+        
+        if (button == &audioFileLoopToogle)
+        {
+            if (readerSource != nullptr)
+            {
+                readerSource->setLooping (audioFileLoopToogle.getToggleState());
+            }
+        }
+        
+    }
+    
+    void changeState (TransportState newState)
+    {
+        if (audioPlayerState != newState)
+        {
+            audioPlayerState = newState;
+            
+            switch (audioPlayerState)
+            {
+                case Stopped:
+                    audioFileStopButton.setEnabled (false);
+                    audioFilePlayButton.setEnabled (true);
+                    transportSource.setPosition (0.0);
+                    break;
+                    
+                case Loaded:
+                    break;
+                    
+                case Starting:
+                    audioFilePlayButton.setEnabled (false);
+                    transportSource.start();
+                    break;
+                    
+                case Playing:
+                    audioFileStopButton.setEnabled (true);
+                    break;
+                    
+                case Stopping:
+                    transportSource.stop();
+                    break;
+            }
+        }
+    }
+    
+    void changeListenerCallback (ChangeBroadcaster* broadcaster) override
+    {
+        if (broadcaster == &transportSource)
+        {
+            if (broadcaster == &transportSource)
+            {
+                if (transportSource.isPlaying())
+                    changeState (Playing);
+                else
+                    changeState (Stopped);
+            }
+        }
+    }
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioInputComponent)
+    
 };
