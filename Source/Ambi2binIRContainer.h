2551f3b7561284cad850ec2ae97fe769522e999b
David Poirier-Quinot <davipoir@gmail.com>
2016-08-17 23:25:39 +0200

    removed .cpp for classes Ambi2binIRContainer, DelayLine, and OSCHandler (header only)

--- Source/Ambi2binIRContainer.h
+++ Source/Ambi2binIRContainer.h
@@ -1,28 +1,91 @@
 #ifndef AMBI2BINIRCONTAINER_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
-
 #include "Utils.h"
-
 #include <array>
 
 class Ambi2binIRContainer
 {
+    
+//==========================================================================
+// ATTRIBUTES
 public:
-    
-    // constructor & destructor
-	Ambi2binIRContainer();
-	~Ambi2binIRContainer();
-
-    // local methods
-    void loadIR(String filename);
-    
-  	// local variables
     std::array< std::array < std::array<float, AMBI2BIN_IR_LENGTH>, 2>, N_AMBI_CH> ambi2binIrDict;
     
-    
-private:
 
+//==========================================================================
+// METHODS
+public:
+
+    Ambi2binIRContainer()
+    {
+        
+        // open HRIR file
+        auto thisDir = File::getSpecialLocation(File::currentExecutableFile).getParentDirectory();
+        
+        DBG(thisDir.getFullPathName());
+        
+        File resourceDir; bool resourceDirDefined = false;
+        if (SystemStats::getOperatingSystemName().startsWithIgnoreCase("Mac"))
+        {
+            resourceDir = thisDir.getParentDirectory().getChildFile("Resources");
+            resourceDirDefined = true;
+        }
+        else if (SystemStats::getOperatingSystemName().startsWithIgnoreCase("Win"))
+        {
+            resourceDir = thisDir.getChildFile("data");
+            resourceDirDefined = true;
+        }
+        else
+        {
+            DBG("OS not handled yet");
+        }
+        
+        if (!resourceDirDefined) // skip loading
+        {
+            DBG("Ambi2bin IR not loaded: resource directory not found");
+        }
+        
+        // load HRIR, ITD, ILD, etc.
+        try
+        {
+            loadIR(resourceDir.getChildFile("hoa2bin_order2_IRC_1008_R_HRIR.bin").getFullPathName());
+        }
+        catch (std::ios_base::failure&)
+        {
+            DBG("Ambi2bin IR not loaded: failed to open file");
+        }
+    }
+    
+    ~Ambi2binIRContainer(){}
+
+    // load ABIRs from file
+    void loadIR(String filename)
+    {
+        FileInputStream istream(filename);
+        if (istream.openedOk())
+        {
+            for (int j = 0; j < N_AMBI_CH; ++j) // loop ambi channels
+            {
+                for (int i = 0; i < AMBI2BIN_IR_LENGTH; ++i) // extract left ear IRs
+                {
+                    istream.read(&ambi2binIrDict[j][0][i], sizeof(float));
+                }
+                
+                for (int i = 0; i < AMBI2BIN_IR_LENGTH; ++i) // extract right ear IRs
+                {
+                    istream.read(&ambi2binIrDict[j][1][i], sizeof(float));
+                }
+            }
+        }
+        else
+            throw std::ios_base::failure("Failed to open ABIR file");
+        
+    }
+    
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Ambi2binIRContainer)
+    
 };
 
 #endif // AMBI2BINIRCONTAINER_H_INCLUDED
--- Source/DelayLine.h
+++ Source/DelayLine.h
@@ -5,33 +5,122 @@
 
 class DelayLine
 {
+
+//==========================================================================
+// ATTRIBUTES
     
 public:
-    
-    DelayLine();
-    ~DelayLine();
-    
-    
-    void setSize(int newNumSamples);
-    void initBufferSize(int newNumSamples);
-    void incrementWritePosition(int numSamples);
-    
-    void addFrom(const AudioBuffer<float> &source, int sourceChannel, int sourceStartSample, int numSamples);
-    AudioBuffer<float> getChunk(int numSamples, int delayInSamples);
-    AudioBuffer<float> getInterpolatedChunk(int numSamples, float delayInSamples);
-
-    AudioBuffer<float> buffer;
-    
     int writeIndex;
-    
+    AudioBuffer<float> buffer;
     AudioBuffer<float> chunkBuffer;
     
 private:
-
     int chunkReadIndex;
-    
     AudioBuffer<float> chunkBufferPrev;
     AudioBuffer<float> chunkBufferNext;
+
+    
+//==========================================================================
+// METHODS
+    
+public:
+    
+    DelayLine()
+    {
+        writeIndex = 0;
+        chunkReadIndex = 0;
+    }
+    
+    ~DelayLine() {}
+    
+    // increase delay line size if need be (no shrinking delay line for now)
+    void setSize(int newNumSamples)
+    {
+        if (newNumSamples > buffer.getNumSamples())
+        {
+            buffer.setSize(1, newNumSamples, true, true);
+        }
+    }
+    
+    // add samples from buffer to delay line
+    void addFrom(const AudioBuffer<float> &source, int sourceChannel, int sourceStartSample, int numSamples)
+    {
+        // either simple copy
+        if ( writeIndex + numSamples <= buffer.getNumSamples() )
+        {
+            buffer.copyFrom(0, writeIndex, source, 0, 0, numSamples);
+        }
+        
+        // or circular copy (last samples of audio buffer will go at delay line buffer begining)
+        else
+        {
+            int numSamplesTail = buffer.getNumSamples() - writeIndex;
+            buffer.copyFrom(0, writeIndex, source, 0, 0, numSamplesTail);
+            buffer.copyFrom(0, 0, source, 0, numSamplesTail, numSamples - numSamplesTail);
+        }
+    }
+    
+    // increment write position, apply circular shift if needed
+    void incrementWritePosition(int numSamples)
+    {
+        writeIndex += numSamples;
+        writeIndex %= buffer.getNumSamples();
+    }
+    
+    void initBufferSize(int newNumSamples)
+    {
+        buffer.setSize(1, newNumSamples);
+        buffer.clear();
+        chunkBuffer.setSize(1, newNumSamples);
+        chunkBuffer.clear();
+        chunkBufferPrev = chunkBuffer;
+        chunkBufferNext = chunkBuffer;
+    }
+    
+    // get delayed buffer out of delay line
+    AudioBuffer<float> getChunk(int numSamples, int delayInSamples)
+    {
+        
+        int writePos = writeIndex - delayInSamples;
+        
+        if ( writePos < 0 )
+        {
+            writePos = buffer.getNumSamples() + writePos;
+            if ( writePos < 0 ) // if after an update the first delay force to go fetch far to far: not best option yet (to set write pointer to zero)
+                writePos = 0;
+        }
+        
+        if ( ( writePos + numSamples ) < buffer.getNumSamples() )
+        { // simple copy
+            chunkBuffer.copyFrom(0, 0, buffer, 0, writePos, numSamples);
+        }
+        else
+        { // circular loop
+            int numSamplesTail = buffer.getNumSamples() - writePos;
+            chunkBuffer.copyFrom(0, 0, buffer, 0, writePos, numSamplesTail );
+            chunkBuffer.copyFrom(0, numSamplesTail, buffer, 0, 0, numSamples - numSamplesTail);
+        }
+        
+        return chunkBuffer;
+    }
+    
+    AudioBuffer<float> getInterpolatedChunk(int numSamples, float delayInSamples)
+    {
+        // get previous and next positions in delay line
+        chunkBufferPrev = getChunk(numSamples, ceil(delayInSamples));
+        chunkBufferNext = getChunk(numSamples, floor(delayInSamples));
+        
+        // apply linear interpolation gains
+        chunkBufferPrev.applyGain((float)(delayInSamples-floor(delayInSamples)));
+        chunkBufferNext.applyGain((float)(ceil(delayInSamples)-delayInSamples));
+        // DBG(String(delayInSamples-floor(delayInSamples)) + String(" ") + String(ceil(delayInSamples)-delayInSamples));
+        
+        // sum buffer in output
+        chunkBuffer = chunkBufferPrev;
+        chunkBuffer.addFrom(0, 0, chunkBufferNext, 0, 0, numSamples);
+        
+        return chunkBuffer;
+    }
     
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayLine)
