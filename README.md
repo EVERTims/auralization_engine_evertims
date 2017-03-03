17b59622063546022429c30145c0b6950c4d2034
David Poirier-Quinot <davipoir@gmail.com>
2017-03-03 21:39:59 +0100

    updated readme, added build osx deps script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# JUCE based Auralization Engine of the EVERTims project
+# EvertSE: Auralization Engine of the EVERTims project
 
-[EVERTims website]: (https://evertims.github.io/website)
+Pre-compiled application may already exist for your architecture, check the install section of the [EVERTims website](https://evertims.github.io/website).  The installation described here targets developers involved in the project.
+
+## Install OSX
+
+* install dependencies: run the ./libs/build_macos.sh script
+* Install [JUCE's Projucer](http://www.juce.com/)
+* Install [XCode](https://developer.apple.com/xcode/)
+* Open ./EvertSE.jucer in Projucer, **from there** open the Xcode project
+* Compile the project in XCode
---
+++ libs/build_macos.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# ----------------
+## init
+# ----------------
+
+# ensure current dir is .
+LIB_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $LIB_DIR
+
+# fetch submodules
+git submodule update --init --recursive
+
+# check for homebrew install
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Suggest to install Homebrew
+    echo 'script aborted: install homebrew to continue (check https://brew.sh/index_fr.html)'
+    exit
+    # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+# ----------------
+## build libmysofa
+# ----------------
+
+# install deps if need be
+for pkg in libresample; do
+    if brew list -1 | grep -q "^${pkg}\$"; then
+        echo "Package '$pkg' already installed"
+    else
+        echo "Installing Package '$pkg'"
+        brew install libresample
+    fi
+done
+
+# compile 
+cd libmysofa/build/
+cmake ..
+make && make install
