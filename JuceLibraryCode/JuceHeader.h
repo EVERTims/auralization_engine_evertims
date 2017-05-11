bf248a26d0419d6c653c95c3aa099c042879cd98
David Poirier-Quinot <davipoir@gmail.com>
2017-05-11 19:56:54 +0200

    updated deps to JUCE 5.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_ZQWLZC__
-#define __APPHEADERFILE_ZQWLZC__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -47,5 +46,3 @@ namespace ProjectInfo
     const int          versionNumber  = 0x10000;
 }
 #endif
-
-#endif   // __APPHEADERFILE_ZQWLZC__
