66ad6d3e630db71024e850b0932200b63ab4cb04
David Poirier-Quinot <davipoir@gmail.com>
2017-07-24 23:07:02 +0200

    updated version 0.3.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,7 +39,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "EvertSE";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.3.2";
+    const int          versionNumber  = 0x302;
 }
 #endif
